068a7092182d3a2c2d8f7e0c1277c457d784d9c9
Ksenia <aki4aaan@mail.ru>
2025-01-05 03:57:57 +0300

    I made an inheritance!

--- HomeworkOOP/src/Car.java
+++ HomeworkOOP/src/Car.java
@@ -5,20 +5,9 @@ public class Car {
     public int horsePower;
     public double engineCapacity;
 
-    public Car(String brand, String model, int numberOfSeat, int horsePower, double engineCapacity) {
-        this.brand = brand;
-        this.model = model;
-        this.numberOfSeat = numberOfSeat;
-        this.horsePower = horsePower;
-        this.engineCapacity = engineCapacity;
-    }
-
-    public String outputInfo() {
+    @Override
+    public String toString() {
         return String.format("Brand: %s \nModel: %s \nNumber of seat: %d \nHorse power: %d \nEngine capacity: %s",
                 brand, model, numberOfSeat, horsePower, engineCapacity);
-    }
-
-    public String drive() {
-        return String.format("vroom-vroom, come sit in my %s %s", brand, model);
     }
 }
